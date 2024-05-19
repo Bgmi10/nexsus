6fe90df2a22f5b1df532ae945af2a6429818a12d
subash <subashchandraboseravi45@gmail.com>
2024-05-18 21:13:20 -0700

    loginform bg fixed

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from 'react';
-import { useHref, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import TextField from '@mui/material/TextField';
@@ -13,8 +13,7 @@ import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import SendIcon from '@mui/icons-material/Send';
 import Button from '@mui/material/Button';
 import bg_icon from '../../src/images/bbb.jpeg';
-import '../component/Home/Home.css'
-
+import '../component/Home/Home.css';
 
 import { useDispatch } from 'react-redux';
 import { login, register } from '../utils/slices/authslice';
@@ -34,8 +33,7 @@ const Signup = () => {
     const [phoneError, setPhoneError] = useState("");
     const [passwordError, setPasswordError] = useState("");
     const navigate = useNavigate();
-    const usehref = useRef()
-   
+    const usehref = useRef();
 
     const handleClickShowPassword = () => {
         setisshowpassword(!isshowPassword);
@@ -47,12 +45,10 @@ const Signup = () => {
 
     const handleRegister = async () => {
         try {
-            // Clear previous error messages
             setEmailError("");
             setPhoneError("");
             setPasswordError("");
 
-            // Perform form field validations
             const isEmailValid = validateEmail(email);
             const isPasswordValid = validatePassword(password);
             const isPhoneValid = validatePhone(phone);
@@ -70,7 +66,6 @@ const Signup = () => {
                 toast.error("Invalid phone number");
             }
 
-            // Proceed with registration if validations pass
             if (isEmailValid && isPasswordValid && isPhoneValid) {
                 const response = await fetch(`${api_rout_url}/api/auth/register`, {
                     method: 'POST',
@@ -86,28 +81,22 @@ const Signup = () => {
                     dispatch(register(user));
                     setIsLogin(true);
                     toast.success("Registration successful");
-                    // Redirect to login page or display success message
                 } else {
                     const data = await response.json();
                     if (data.message) {
-                        // Set error message in state to display in UI
                         setEmailError(data.message);
                         toast.error(data.message);
                     }
                     console.error("Registration failed");
-                    // Handle registration failure (e.g., display error message)
                 }
             }
         } catch (error) {
             console.error("Error during registration:", error.message);
             toast.error("Error during registration");
-            // Handle registration error (e.g., display error message)
         }
     };
 
-    // In your authentication logic after successful login
-    // Modify your login function to store the user ID in session storage
-    const handleLogin = async (userData) => {
+    const handleLogin = async () => {
         try {
             const response = await fetch(`${api_rout_url}/api/auth/login`, {
                 method: 'POST',
@@ -119,27 +108,20 @@ const Signup = () => {
 
             if (response.ok) {
                 const data = await response.json();
-                console.log(data)
                 const id = data.user._id;
-                 
-                // Store the user ID in session storage
                 sessionStorage.setItem('userId', id);
 
                 const userRole = data.user.role;
-                dispatch(login(data.user._id))
+                dispatch(login(data.user._id));
 
-                // Redirect logic based on user role
                 if (userRole === "admin") {
                     window.location.href = "/dashboard/admin";
                 } else if (userRole === "user") {
                     window.location.href = "/dashboard/user";
                 } else {
-                    // Role neither admin n                    // Show message or handle redirection as per your requirement
                     console.log("Please contact admin to update your role");
                 }
             } else {
-                // Handle login error
-               
                 console.error("Login failed:", response.statusText);
                 toast.error("Login failed");
             }
@@ -147,77 +129,101 @@ const Signup = () => {
             console.error("Error during login:", error);
             toast.error("Error during login");
         }
-    };  
+    };
 
-    
-    const isimg = window.innerWidth >= 786
+    const isimg = window.innerWidth >= 786;
 
     return (
-        <div  className={ isimg ? `flex justify-center items-center h-screen   bg-center bg-cover  bg-black ` :  `flex justify-center items-center h-screen  ml-10 bg-center bg-cover  bg-black  ` } >
-           
-            <ToastContainer />
-            
-            <p className='absolute mb-[390px] text-white font-bold  text-2xl mr-6'>{isLogin ? "Log in" : "Register"}</p>
-            <div className="p-8 rounded-md shadow-md  ">
-                {!isLogin && (
-                    <>
-                        <div className="mb-4 ">
+        <div className={`relative flex justify-center items-center h-screen bg-black`} style={{ backgroundImage: `url(${bg_icon})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+            <div className="absolute inset-0 bg-black opacity-50"></div> {/* Overlay with reduced opacity */}
+            <div className="relative z-10 p-8 rounded-lg shadow-md bg-opacity-70 bg-black">
+                <ToastContainer />
 
-                            <TextField id="outlined-required"     label="name"  variant="outlined" fullWidth onChange={(e) => setname(e.target.value)}  className='bg-white text-gray-500 rounded-md'/>
-                        </div>
-                        <div className="mb-4">
-                            <TextField required id="outlined-required" label="Phone Number" variant="outlined" fullWidth onChange={(e) => setphone(e.target.value)}  className='bg-white text-gray-500 rounded-md'/>
-                            {phoneError && <p className="text-red-500 text-xs">{phoneError}</p>}
-                        </div>
-                    </>
-                )}
-                <div className="mb-4">
-                    <TextField required id="outlined-required" label="E-mail" variant="outlined" size='large' fullWidth onChange={(e) => setemail(e.target.value)}  className='bg-white text-gray-500 rounded-md' />
-                    {emailError && <p className="text-red-500 text-xs">{emailError}</p>}
-                </div>
-                <div className="mb-4">
-    <FormControl fullWidth  className='ml-10' >
-        <InputLabel htmlFor="outlined-adornment-password"  className=' text-gray-500 rounded-md'>Password</InputLabel>
-        <OutlinedInput
-            id="outlined-adornment-password"
-            className='bg-white text-gray-500 rounded-md'
-            type={isshowPassword ? 'text' : 'password'}
-            endAdornment={
-                <InputAdornment>
-                    <IconButton
-                        aria-label="toggle password visibility"
-                        onClick={handleClickShowPassword}
-                        onMouseDown={handleMouseDownPassword}
-                        edge="start"
-                    
-                    >
-                        {!isshowPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                    
-                </InputAdornment>
-            }
-           
-            label="Password"
-            onChange={(e) => setpassword(e.target.value)}
-            
-        />
-    </FormControl>
-    {passwordError && <p className="text-red-500 text-xs">{passwordError}</p>}
-</div>
+                <p className='absolute mt-[-28px] text-white font-bold text-2xl ml-28  '>{isLogin ? "Log in" : "Register"}</p>
+                <div className="p-8 rounded-md shadow-md bg-opacity-40 bg-black">
+                    {!isLogin && (
+                        <>
+                            <div className="mb-4">
+                                <TextField
+                                    id="outlined-required"
+                                    label="Name"
+                                    variant="outlined"
+                                    fullWidth
+                                    onChange={(e) => setname(e.target.value)}
+                                    className='bg-white text-gray-500 rounded-md'
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <TextField
+                                    required
+                                    id="outlined-required"
+                                    label="Phone Number"
+                                    variant="outlined"
+                                    fullWidth
+                                    onChange={(e) => setphone(e.target.value)}
+                                    className='bg-white text-gray-500 rounded-md'
+                                />
+                                {phoneError && <p className="text-red-500 text-xs">{phoneError}</p>}
+                            </div>
+                        </>
+                    )}
+                    <div className="mb-4">
+                        <TextField
+                            required
+                            id="outlined-required"
+                            label="E-mail"
+                            variant="outlined"
+                            size='large'
+                            fullWidth
+                            onChange={(e) => setemail(e.target.value)}
+                            className='bg-white text-gray-500 rounded-md'
+                        />
+                        {emailError && <p className="text-red-500 text-xs">{emailError}</p>}
+                    </div>
+                    <div className="mb-4">
+                        <FormControl fullWidth className='ml-10'>
+                            <InputLabel htmlFor="outlined-adornment-password" className='text-gray-500 rounded-md'>Password</InputLabel>
+                            <OutlinedInput
+                                id="outlined-adornment-password"
+                                className='bg-white text-gray-500 rounded-md'
+                                type={isshowPassword ? 'text' : 'password'}
+                                endAdornment={
+                                    <InputAdornment>
+                                        <IconButton
+                                            aria-label="toggle password visibility"
+                                            onClick={handleClickShowPassword}
+                                            onMouseDown={handleMouseDownPassword}
+                                            edge="start"
+                                        >
+                                            {!isshowPassword ? <VisibilityOff /> : <Visibility />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                                label="Password"
+                                onChange={(e) => setpassword(e.target.value)}
+                            />
+                        </FormControl>
+                        {passwordError && <p className="text-red-500 text-xs">{passwordError}</p>}
+                    </div>
 
-                <h1 className='text-[11px] cursor-pointer hover:text-gray-400 text-gray-300' onClick={ ()=>setIsLogin(!isLogin)}>
-                    {isLogin ? "New to Nexus ?" : "Already have an account ?"}
-                </h1>
-                <div className='mb-4 mt-3 bg-black'>
-                    <Button variant="contained" size="large" fullWidth endIcon={<SendIcon />} onClick={isLogin ? handleLogin : handleRegister}  className='text-black '>
-                        {isLogin ? "Login" : "Register"}
-                    </Button>
+                    <h1 className='text-[11px] cursor-pointer hover:text-gray-400 text-gray-300' onClick={() => setIsLogin(!isLogin)}>
+                        {isLogin ? "New to Nexus ?" : "Already have an account ?"}
+                    </h1>
+                    <div className='mb-4 mt-3'>
+                        <Button
+                            variant="contained"
+                            size="large"
+                            fullWidth
+                            endIcon={<SendIcon />}
+                            onClick={isLogin ? handleLogin : handleRegister}
+                            className='text-black'
+                        >
+                            {isLogin ? "Login" : "Register"}
+                        </Button>
+                    </div>
                 </div>
             </div>
-
-           
         </div>
-        
     );
 };
 
